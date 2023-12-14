fbae987a7aa67a67188b41bb707599d2786f1f3c
Hai HUANG <101720804+maple367@users.noreply.github.com>
2023-12-14 12:00:57 +0800

    Revert
    This reverts commit d00321b8dee9b743b2bc3f4ffcd1845760ca877c.

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          插件功能已更新，欢迎使用。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
