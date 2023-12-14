3ddf2ed83442c7c3e45d56f90a1dd13d5c565391
Hai HUANG <101720804+maple367@users.noreply.github.com>
2023-12-14 14:21:22 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -159,6 +159,7 @@ export function SideBar(props: { className?: string }) {
         </div>
         <div className={styles["sidebar-sub-title"]}>
           插件功能已更新，欢迎使用。输入搜索提示语可在线搜索。DALL·E比较贵，酌情使用。
+          插件功能会大量消耗token，不要用gpt-4调用插件。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
