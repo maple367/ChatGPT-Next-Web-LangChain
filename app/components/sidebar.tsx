5f6018648db3d44f46812185198cb425008d4fef
Hai HUANG <101720804+maple367@users.noreply.github.com>
2023-12-15 15:22:02 +0800

    modified:   app/components/sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          GPT FOR NOEL
         </div>
         <div className={styles["sidebar-sub-title"]}>
           插件功能已更新，欢迎使用。输入搜索提示语可在线搜索。DALL·E比较贵，酌情使用（可以在面具里选择“以文搜图”代替）。GPT-4价格是3.5的10倍！！！
