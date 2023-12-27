705797228fefc75e4c947d5190e88c763f135052
Hai HUANG <101720804+maple367@users.noreply.github.com>
2023-12-27 11:59:54 +0800

    update title

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -538,7 +538,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>GPT FOR NOEL</div>
             <div className={styles["sub-title"]}>
               github.com/Yidadaa/ChatGPT-Next-Web
             </div>
